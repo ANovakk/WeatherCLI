2de5abeb3fa924be098dd711fccc1e68955cb5b4
Andrey Novak <andreynovak2005@gmail.com>
2025-01-29 12:13:06 +0100

    Add options: wind, precipitation, humidity, pressure, cloud cover, gusts

--- src/weather_cli/commands/now.py
+++ src/weather_cli/commands/now.py
@@ -70,7 +70,14 @@ def find_coordinates(city, district):
 @click.option("--district", default=None, help="District/area name (optional)")
 @click.option( '--days', '-d', '--number', '-n', default=[1], show_default=True,
               help="Number of days to show [1-7]")
-def now(city, district, days):
+@click.option('--wind', '-w', is_flag=True, help="Include wind speed (optional)")
+@click.option('--precipitation', '-p', is_flag=True, help="Include precipitation (optional)")
+@click.option('--humidity', '-h', is_flag=True, help="Include humidity (optional)")
+@click.option('--pressure', is_flag=True, help="Include pressure (optional)")
+@click.option('--cloud', '-c', is_flag=True, help="Include cloud cover (optional)")
+@click.option('--gust', '-g', is_flag=True, help="Include maximum wind speed and gusts (optional)")
+def now(city, district, days, wind,
+        precipitation, humidity, pressure, cloud, gust, uv):
     """Get coordinates by city name and/or district"""
     data = find_coordinates(city, district)
 
@@ -86,11 +93,23 @@ def now(city, district, days):
     click.echo("\n".join(output))
 
     try:
+        get_wind_value = "wind_speed_10m" if wind else None
+        get_precipitation = "precipitation_probability" if precipitation else None
+        get_humidity = "relative_humidity_2m" if humidity else None
+        get_pressure = "pressure_msl" if pressure else None
+        get_cloud_cover = "cloud_cover" if cloud else None
+        get_wind_gusts = "wind_gusts_10m" if gust else None
         params = {
             "latitude": data['lat'],
             "longitude": data['lon'],
-            "hourly": "temperature_2m",
-            "forecast_days": max(1, min(7, days))
+            "hourly": ["temperature_2m",
+                       get_wind_value,
+                       get_precipitation,
+                       get_humidity,
+                       get_pressure,
+                       get_cloud_cover,
+                       get_wind_gusts],
+            "forecast_days": max(1, min(7, days)),
         }
         response = requests.get(API_URL_OPEN_METEO, params=params)
         response.raise_for_status()
